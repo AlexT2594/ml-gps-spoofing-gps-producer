1070ccee313cb637607029852873a7b35f340702
Alex Tufa <alex.tufa94@gmail.com>
2019-03-07 14:49:54 +0100

    Data arrives at the broker in the right manner

--- apis/raw_nmea_producer.py
+++ apis/raw_nmea_producer.py
@@ -37,13 +37,52 @@ class RawNmeaProducerAPI:
         res.content_type = falcon.MEDIA_JSON
         res.status = falcon.HTTP_200
 
-        if message_ID == "$PGLOR":
+        # we ignore not useful phrases
+        if message_ID != "$GPGGA" and message_ID != "$GPGSA" and message_ID != "$GPGSV" and message_ID != "$GPRMC":
             res.media = result
             return
         elif message_ID == "$GPRMC":
             self.entry.add(phrase)
             self.entry = list(self.entry)
-            self.entry.sort(key=lambda phrase: phrase[3:6])
+            self.entry.sort(key=lambda phrase: phrase[:10]) #[:10] because we want to sort also the GSV phrases
+            
+            print("==> Entry array")
+            print(self.entry)
+
+            #we need to check that all $GPGSV entries are present
+            for entry_index in range(len(self.entry)):
+                current_phrase = self.entry[entry_index]
+                if current_phrase[:6] == "$GPGSV":
+                    current_phrase_array = current_phrase.split(",")
+                    total_GSV_messages = int(current_phrase_array[1])
+
+                    for GSV_index in range(entry_index, entry_index + total_GSV_messages):
+                        # if the GSV_index reaches the end then some phrases are missing
+                        if GSV_index >= len(self.entry):
+                            self.entry = set()
+                            return
+                        # if this phrase is not GSV then for sure some GSV message is missing
+                        # order is verified by the sorting function
+                        elif self.entry[GSV_index][:6] != "$GPGSV":
+                            self.entry = set()
+                            return
+
+                    # if I reach this point then I don't need to do other checks
+                    break
+            
+            # we need to remove some particular duplicates
+            to_remove_indices = list()
+            for entry_index in range(len(self.entry)):
+                current_phrase = self.entry[entry_index]
+                if current_phrase[:6] == "$GPGSV" and entry_index - 1 >= 0:
+                    previous_phrase = self.entry[entry_index - 1]
+
+                    if previous_phrase[:6] == "$GPGSV" and current_phrase[9:10] == previous_phrase[9:10]:
+                        to_remove_indices.append(entry_index)
+
+            for index in to_remove_indices:
+                del self.entry[index]
+            
             result_entry = ";".join(self.entry)
 
             #if the entry doesn't contain at least the phrase $GPGGA we don't
@@ -57,7 +96,6 @@ class RawNmeaProducerAPI:
                 self.entry = set()
                 return
 
-            print(result_entry)
             kafka_producer = connect_kafka_producer()
             publish_message(kafka_producer, 'raw_nmea', 'raw_nmea_message', result_entry)
 
